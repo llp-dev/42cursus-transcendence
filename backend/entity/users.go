f41bb1380cb50231140e7d3531fb3ce2af2c7b6b
luluzuri <luluzuri@student.42.fr>
2026-03-23 19:02:57 +0100

    feat[backend]: Added file tree

---
+++ backend/entity/users.go
@@ -0,0 +1,10 @@
+package entity
+
+type Users struct {
+	ID          int
+	Pseudo      string `json:"title"`
+	First_name  string `json:"first_name"`
+	Last_name   string `json:"last_name"`
+	Description string `json:"description"`
+	ImageUrl    string `json:"image_url"`
+}
