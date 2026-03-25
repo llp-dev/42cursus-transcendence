fd733b8a8a5e3f207d80943dd62571d6416d3040
luluzuri <luluzuri@student.42.fr>
2026-03-25 15:15:53 +0100

    feat[all]: Trying and testing go and initializing go module and gin

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if (err != nil) {
+		log.Println("[WARNING] .env file not found")
+	}
+
 	var router *gin.Engine= gin.Default()
+	router.SetTrustedProxies(nil)
 
 	router.GET("/", func(ctx *gin.Context) {
-		
+		ctx.JSON(200, gin.H{
+			// gin.H = map[string]any{}
+			"message": "Backend API is running",
+			"status": "success",
+		})
 	})
+
+	port := os.Getenv("API_PORT")
+	if (port == "") {
+		port = ":8000"
+	}
+
+	router.Run(":" + port)
 }
