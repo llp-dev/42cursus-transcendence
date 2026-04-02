aa291aa7a04d6531726e357a45f08a70e277a7d2
vali <li.valentin2@gmail.com>
2026-04-02 15:23:14 +0200

    feat(models): add User struct with JSON tags and time fields

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,1 +1,16 @@
 package models
+
+import "time"
+
+type User struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Wallpaper []byte    `json:"wallpaper"`
+	Avatar    []byte    `json:"avatar"`
+	Bio       string    `json:"bio"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
+}
