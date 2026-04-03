bee0e0d7c8c5676139b7d1e53462d06eb3803c0b
vali <li.valentin2@gmail.com>
2026-04-03 16:03:19 +0200

    Fix(db data): update of the database and migration

--- database/init.sql
+++ database/init.sql
@@ -1,14 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    name TEXT,
-    username TEXT UNIQUE,
-    email TEXT UNIQUE,
-    bio TEXT,
-    wallpaper BYTEA,
-    avatar BYTEA,
-    created_at TIMESTAMP DEFAULT NOW(),
-    updated_at TIMESTAMP DEFAULT NOW(),
-    deleted_at TIMESTAMP
+    id			UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    name		TEXT NOT NULL,
+    username	TEXT NOT NULL UNIQUE,
+    password	TEXT NOT NULL,
+    email		TEXT NOT NULL UNIQUE,
+    bio			TEXT,
+    wallpaper	TEXT,
+    avatar		TEXT,
+    created_at	TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at	TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted_at	TIMESTAMP
 );
