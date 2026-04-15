b37f4197599c693f3bfd7ef3eb163889b2f69bfc
luluzuri <luluzuri@student.42.fr>
2026-04-15 15:56:54 +0200

    feature[]: Added the main structure (may need a refractor)

--- backend/models/post.go
+++ backend/models/post.go
@@ -1,1 +1,32 @@
 package models
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+type Post struct {
+    ID        string    `gorm:"primaryKey;type:varchar(36)"`
+    AuthorID  string    `gorm:"type:varchar(36);not null"`
+    Author    User      `gorm:"foreignKey:AuthorID"`
+    Content   string    `gorm:"type:text;not null"`
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    DeletedAt gorm.DeletedAt `gorm:"index"`
+
+    LikesCount    int    `json:"likes_count" gorm:"default:0"`
+    CommentsCount int    `json:"comments_count" gorm:"default:0"`
+}
+
+type UpdatePostInput struct {
+    Content string `json:"content" binding:"required,max=280"`
+}
+
+type PostResponse struct {
+    ID        string       `json:"id"`
+    Content   string       `json:"content"`
+    AuthorID  string       `json:"author_id"`
+    Author    UserResponse `json:"author"`
+    CreatedAt time.Time    `json:"created_at"`
+    UpdatedAt time.Time    `json:"updated_at"`
+}
