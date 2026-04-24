75e90d6533bce243f8e37e586cae853ca2162bed
vali <vali@student.42.fr>
2026-04-24 08:34:06 +0200

    add(models): new model for Message

--- backend/models/message.go
+++ backend/models/message.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+type Message struct {
+	ID        string    `gorm:"primaryKey;type:varchar(36)" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	SenderID  string    `json:"sender_id" gorm:"not null"`
+	RoomID    string    `json:"room_id" gorm:"not null"`
+	Content   string    `json:"content" gorm:"not null"`
+	ParentID  *string   `json:"parent_id" gorm:"default:null"`
+	Replies   []Message `json:"replies,omitempty" gorm:"foreignKey:ParentID"`
+}
