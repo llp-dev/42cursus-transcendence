ada0d1d5ecfc02a6669c28d6ddbba738e48c34f9
vali <vali@student.42.fr>
2026-04-24 13:16:29 +0200

    merge with lastest version of DEV

--- backend/socket/ws_manager.go
+++ backend/socket/ws_manager.go
@@ -1,1 +1,31 @@
 package websocket
+
+import (
+	"log"
+	"sync"
+
+	"github.com/Transcendence/models"
+)
+
+type WSManager struct {
+	mu      sync.RWMutex
+	rooms   map[string]map[string]*models.Client
+	clients map[string]*models.Client
+}
+
+func NewWSManager() *WSManager {
+	return &WSManager{
+		rooms:   make(map[string]map[string]*models.Client),
+		clients: make(map[string]*models.Client),
+	}
+}
+
+func (m *WSManager) RegisterClient(client *models.Client) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.clients[client.ID] = client
+	log.Printf("Client %s connected\n", client.ID)
+}
+
+
